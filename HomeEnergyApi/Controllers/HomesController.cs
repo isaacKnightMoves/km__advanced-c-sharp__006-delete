404f4ac4851397836aac35b37342647b4ba1a7bb
isaacKnightMoves <162600852+isaacKnightMoves@users.noreply.github.com>
2025-01-20 12:43:33 -0600

    Initial commit

---
+++ HomeEnergyApi/Controllers/HomesController.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeEnergyUsageApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomesController : ControllerBase
+    {
+        private static List<Home> homesList = new List<Home>();
+
+        [HttpGet]
+        public IEnumerable<Home> Get()
+        {
+            return homesList;
+        }
+
+        [HttpGet("{id}")]
+        public Home FindById(int id)
+        {
+            foreach (Home home in homesList)
+            {
+                if (home.id == id)
+                    return home;
+            }
+            return null;
+        }
+
+        [HttpPost]
+        public Home Post([FromBody] Home home)
+        {
+            homesList.Add(home);
+            return home;
+        }
+
+        [HttpPut("{id}")]
+        public Home Put([FromBody] Home newHome, [FromRoute] int id)
+        {
+            for (int i = 0; i < homesList.Count; i++)
+            {
+                if (homesList[i].id == id)
+                {
+                    homesList[i] = newHome;
+                    return newHome;
+                }
+            }
+            return null;
+        }
+    }
+}
